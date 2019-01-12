ae74af644f4cc40f670522d2e5b421721871673f
Sarvesh Athawale <sathawale27@gmail.com>
2019-01-12 15:27:09 +0530

    Initial commit

---
+++ app/src/main/java/com/sarveshathawale/pocketgitclient/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.sarveshathawale.pocketgitclient
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
